904c4d7e952d8afad16ba2860583f2f4e543e19a
nguyenquy549 <nguyenquy549@gmail.com>
2025-08-13 17:19:40 +0100

    chmod 777 all file

--- Production/build/serial/serial_install.sh
+++ Production/build/serial/serial_install.sh
@@ -3,7 +3,6 @@
 echo "***** Setup Serial Config  Start *****" 
 BASEDIR=$(dirname "$SCRIPT")
 
-// copy and reply
-sudo cp -rf $BASEDIR/config.txt  /boot/firmware/
+sudo cp $BASEDIR/config.txt  /boot/firmware/
 
 echo "***** Setup Serial Config  End *****"
