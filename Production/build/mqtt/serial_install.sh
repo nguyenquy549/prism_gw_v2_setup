10730668ec25a0d0962889de3098983cd2dadb12
quyn <quyn@infinitepl.com>
2025-08-13 23:47:20 +0700

    add verbose output for mosquitto installation

--- Production/build/mqtt/serial_install.sh
+++ Production/build/mqtt/serial_install.sh
@@ -4,6 +4,7 @@ echo "***** Setup Serial Config  Start *****"
 BASEDIR=$(dirname "$SCRIPT")
 sudo apt install -y mosquitto mosquitto-clients
 sudo systemctl enable mosquitto.service
+mosquitto -v
 sudo cp $BASEDIR/mosquitto.conf  /boot/firmware/
 sudo systemctl restart mosquitto
 sudo systemctl status mosquitto
