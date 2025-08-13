ecd5ad4b9b459b3899c902389ae3d09478af7ff7
quyn <quyn@infinitepl.com>
2025-08-13 23:48:22 +0700

    add mosquitto configuration and serial installation script

--- Production/build/mosquitto/serial_install.sh
+++ Production/build/mosquitto/serial_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "***** Setup Serial Config  Start *****" 
+echo "***** Setup mosquitto Config  Start *****" 
 BASEDIR=$(dirname "$SCRIPT")
 sudo apt install -y mosquitto mosquitto-clients
 sudo systemctl enable mosquitto.service
@@ -8,4 +8,4 @@ mosquitto -v
 sudo cp $BASEDIR/mosquitto.conf  /boot/firmware/
 sudo systemctl restart mosquitto
 sudo systemctl status mosquitto
-echo "***** Setup Serial Config  End *****"
+echo "***** Setup mosquitto Config  End *****"
