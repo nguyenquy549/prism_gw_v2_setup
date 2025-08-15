2ac2138dab6282dbb72fd0fc566469cedc9add62
quyn <quyn@vng.com.vn>
2025-08-15 12:43:28 +0700

    Add mosquitto installation script and update full_install.sh to call it

--- full_install.sh
+++ full_install.sh
@@ -3,13 +3,13 @@ SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 sudo chmod -R 777 $BASEDIR/Production
 echo "***** Install Gateway V2 End *****"
-# sudo apt-get update
+sudo apt-get update
 sudo apt-get install build-essential git libssl-dev
 sudo apt install libjson-c-dev
 sudo apt-get install libcurl4-openssl-dev
 sudo apt-get install libsqlite3-dev
 sudo apt install build-essential libpython3-dev libdbus-1-dev
-$BASEDIR/Production/build/mqtt/serial_install.sh
+$BASEDIR/Production/build/mosquitto/mosquitto_install.sh
 $BASEDIR/Production/build/paho.mqtt.c/paho_install.sh
 $BASEDIR/Production/build/serial/serial_install.sh
 $BASEDIR/Production/install/install.sh
